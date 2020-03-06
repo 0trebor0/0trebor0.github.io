20b8d1164e61bc50070796b74f64126a5c95c6d3
robert <webdevme@outlook.com>
2020-03-06 00:25:44 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+element = ( id = null, from = null )=>{
+    var e = {};
+    if( id !== null ){
+        e.dom = document.querySelector( id );
+    }else if( id !== null || from !== null ){
+        e.dom = from.querySelector( id );
+    }
+    e.html = e.dom.innerHTML;
+    e.style = e.dom.style;
+    e.element = ( id )=>{
+        return element( id, e.html );
+    };
+    return e;
+}
