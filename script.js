11e78807009d81ce6829f230d5c0770d876cecec
robert <webdevme@outlook.com>
2020-03-10 12:26:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-element = ( id = null, from = null )=>{
+element = ( id = null )=>{
     var e = null;
-    if( id !== null && from !== null ){
-        if( from.querySelectorAll( id ).length > 1 ){
-            e = from.querySelectorAll( id );
-        } else {
-            e = from.querySelector( id );
-        }
-    }else if( id !== null ){
+    if( id !== null ){
         if( document.querySelectorAll( id ).length > 1 ){
             e = document.querySelectorAll( id );
         } else {
             e = document.querySelector( id );
-        }
-    }
-    if( e !== null ){
-        if( e.length > 1 ){
-            e.forEach( (o)=>{
-                o.child = ( id )=>{
-                    return element( id, new DOMParser().parseFromString(o.innerHTML, 'text/html') );
-                }
-            } );
-        } else {
-            e.child = ( id )=>{
-                return element( id, new DOMParser().parseFromString(e.innerHTML, 'text/html') );
-            }
         }
     }
     return e;
