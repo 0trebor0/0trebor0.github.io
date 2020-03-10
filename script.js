8594f257604c168b77bcacbeee05f1e379d460ea
robert <webdevme@outlook.com>
2020-03-10 11:02:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-
-element = ( id = null, from = null )=>{
+element = ( id = null )=>{
     var e = null;
     if( id !== null ){
-        e = document.querySelector( id );
-    }else if( id !== null || from !== null ){
-        e = from.querySelector( id );
-    }
-    if( e !== null ){
-        e.child = ( id )=>{
-            return element( id, e.innerHTML );
-        };
+        e = document.querySelectorAll( id );
     }
     return e;
 }
