6c3fb2464cb488abe5027fc729fe42341b45d063
robert <webdevme@outlook.com>
2020-03-12 13:14:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.querySelectorAll( 'input[type=text], input[type=password]' ).forEach( ( input )=>{
+    if( input !== null ){
+        input.autocomplete = "off";
+    }
+} );
 element = ( id = null, from = null )=>{
     var e = null;
     if( from !== null && id !== null ){
