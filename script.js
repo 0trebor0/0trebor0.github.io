3b32c8844d07b9a3e74915a175e2728dd8052feb
robert <webdevme@outlook.com>
2020-03-12 13:15:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.querySelectorAll( 'input[type=text], input[type=password]' ).forEach( ( input )=>{
-    if( input !== null ){
-        input.autocomplete = "off";
-    }
-} );
+window.onload = ()=>{
+    document.querySelectorAll( 'input[type=text], input[type=password]' ).forEach( ( input )=>{
+        if( input !== null ){
+            input.autocomplete = "off";
+        }
+    } );
+};
 element = ( id = null, from = null )=>{
     var e = null;
     if( from !== null && id !== null ){
