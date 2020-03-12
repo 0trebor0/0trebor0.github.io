0432b60f9e83ee84190c4691aea0f1ade0a482e1
robert <webdevme@outlook.com>
2020-03-12 00:28:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-element = ( id = null )=>{
+element = ( id = null, from = null )=>{
     var e = null;
-    if( id !== null ){
+    if( from !== null && id !== null ){
+        let test = new DOMParser().parseFromString( from ,'text/html' );
+        if( test.querySelectorAll( id ).length > 1 ){
+            e = test.querySelectorAll( id );
+            e.forEach( ( g )=>{
+                g.element = ( id )=>{
+                    return element( id, g.innerHTML );
+                }
+            } );
+        } else {
+            e = test.querySelector( id );
+            e.element = ( id )=>{
+                return element( id, e.innerHTML );
+            }
+        }
+    }else if( id !== null ){
         if( document.querySelectorAll( id ).length > 1 ){
             e = document.querySelectorAll( id );
+            e.forEach( ( g )=>{
+                g.element = ( id )=>{
+                    return element( id, g.innerHTML );
+                }
+            } );
         } else {
             e = document.querySelector( id );
+            e.element = ( id )=>{
+                return element( id, e.innerHTML );
+            }
         }
     }
     return e;
