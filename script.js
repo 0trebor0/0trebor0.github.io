bf6eb2c1ad3a01fbd3d26d823cf52409dab2d277
0trebor0 <public0projects@outlook.com>
2020-05-09 21:20:29 +0100

    commit

--- script.js
+++ script.js
@@ -41,6 +41,25 @@ element = ( id = null, from = null )=>{
     }
     return e;
 }
+class Github{
+    constructor( username = null ){
+        this.url = "https://api.github.com";
+        this.repos = {};
+        this.user = {};
+        this.username;
+        if( username !== null ){
+            this.username = username;
+        }
+    }
+    async getRepos(){
+        this.repos = await fetch( this.url+"/users/"+this.username+"/repos" );
+        this.repos = await this.repos.json();
+    }
+    async getUser(){
+        this.user = await fetch( this.url+"/users/"+this.username);
+        this.user = await this.user.json();
+    }
+}
 class Peers{
     constructor( config = null ){
         this.peers = {};
