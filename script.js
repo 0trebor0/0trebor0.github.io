a7fe19ec27d658d83e3198b901b0fefb628d8740
0trebor0 <public0projects@outlook.com>
2020-07-31 13:42:19 +0100

    fixing an error

--- script.js
+++ script.js
@@ -188,37 +188,37 @@ var d = (d)=>{
                     } else {
                         u = {'type':'error','msg':'cant find '+json.parent};
                     }
-                    if( json.attribute && typeof json.attribute == 'object' ){
-                        for( a in json.attribute ){
-                            u[a] = json.attribute[a];
+                }
+                if( json.attribute && typeof json.attribute == 'object' ){
+                    for( a in json.attribute ){
+                        u[a] = json.attribute[a];
+                    }
+                }
+                if( json.class && json.class.length > 0){
+                    //Loop to add class
+                    json.class.forEach((c)=>{
+                        u.classList.add(c);
+                    });
+                }
+                if( json.onclick && typeof json.onclick == 'function'){
+                    //Add onclick event
+                    u.onclick = json.onclick;
+                }
+                if( json.oncreate && typeof json.oncreate == 'function' ){
+                    //Run function on element create
+                    json.oncreate(u);
+                }
+                if( json.body && typeof json.body == 'string' ){
+                    u.innerHTML += json.body;
+                } else if( json.body && typeof json.body == 'object' && json.body.length > 0 ){
+                    json.body.forEach((b)=>{
+                        if( typeof b == 'object' ){
+                            b.parent = u;
+                            methods.create(b);
+                        } else if( typeof b == 'string' ){
+                            u.innerHTML += b;
                         }
-                    }
-                    if( json.class && json.class.length > 0){
-                        //Loop to add class
-                        json.class.forEach((c)=>{
-                            u.classList.add(c);
-                        });
-                    }
-                    if( json.onclick && typeof json.onclick == 'function'){
-                        //Add onclick event
-                        u.onclick = json.onclick;
-                    }
-                    if( json.oncreate && typeof json.oncreate == 'function' ){
-                        //Run function on element create
-                        json.oncreate(u);
-                    }
-                    if( json.body && typeof json.body == 'string' ){
-                        u.innerHTML += json.body;
-                    } else if( json.body && typeof json.body == 'object' && json.body.length > 0 ){
-                        json.body.forEach((b)=>{
-                            if( typeof b == 'object' ){
-                                b.parent = u;
-                                methods.create(b);
-                            } else if( typeof b == 'string' ){
-                                u.innerHTML += b;
-                            }
-                        });
-                    }
+                    });
                 }
 
             } else {
